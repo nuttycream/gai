8f807ddbdc48030be4496b765d597a5043a292e7
nuttyCream <issanutty@gmail.com>
2025-09-12 10:06:53 -0700

    initial

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "sussg";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with pkgs; {
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            name = "gai";
+            src = ./.;
+            buildInputs = [];
+            nativeBuildInputs = [];
+            cargoHash = lib.fakeHash;
+          };
+
+          devShells.default = mkShell {
+            name = "gai";
+            packages = with pkgs; [
+              rust-bin.stable.latest.minimal
+              rust-analyzer
+              rustfmt
+            ];
+
+            buildInputs = [];
+          };
+        }
+    );
+}
