0f8d8b9c0c4763f9614b8d0c6cef1157446a2ba1
nuttyCream <issanutty@gmail.com>
2025-10-07 23:29:48 -0700

    refactor(git-repo): update GaiGit to use a public repo field and remove direct usage of Commit; remove apply_commits/commit from GaiGit implementation

--- src/git/repo.rs
+++ src/git/repo.rs
@@ -2,16 +2,14 @@ use anyhow::{Result, bail};
 use git2::{
     DiffHunk, DiffLine, DiffOptions, Repository, StatusOptions,
 };
-use std::{collections::HashMap, path::Path};
+use std::collections::HashMap;
 use walkdir::WalkDir;
-
-use crate::{ai::response::Commit, config::Config};
 
 pub struct GaiGit {
     /// file name, (should truncate? and vec of hunks)
     pub files: Vec<GaiFile>,
 
-    repo: Repository,
+    pub repo: Repository,
 }
 
 pub struct GaiFile {
@@ -213,78 +211,6 @@ impl GaiGit {
         }
 
         file_diffs
-    }
-
-    pub fn apply_commits(&self, commits: &[Commit], cfg: &Config) {
-        //println!("{:#?}", self.commits);
-        for commit in commits {
-            self.commit(commit, cfg);
-        }
-    }
-
-    fn commit(&self, commit: &Commit, cfg: &Config) {
-        let mut index = self.repo.index().unwrap();
-
-        index.clear().unwrap();
-
-        if let Ok(head) = self.repo.head()
-            && let Ok(tree) = head.peel_to_tree()
-        {
-            index.read_tree(&tree).unwrap();
-        }
-
-        // staging
-        for path in &commit.files {
-            let path = Path::new(&path);
-            let status = self.repo.status_file(path).unwrap();
-
-            // todo: some changes will implement a combo
-            // ex: modified + renamed
-            // i think we need to explicitly handle those
-            // maybe by storing it in a buffer of some sort
-            if status.contains(git2::Status::WT_MODIFIED)
-                || status.contains(git2::Status::WT_NEW)
-            {
-                index.add_path(path).unwrap();
-            }
-            if status.contains(git2::Status::WT_DELETED) {
-                index.remove_path(path).unwrap();
-            }
-            if status.contains(git2::Status::WT_TYPECHANGE) {
-                index.remove_path(path).unwrap();
-                index.add_path(path).unwrap();
-            }
-        }
-
-        index.write().unwrap();
-
-        let tree_oid = index.write_tree().unwrap();
-        let tree = self.repo.find_tree(tree_oid).unwrap();
-
-        let parent_commit = match self.repo.revparse_single("HEAD") {
-            Ok(obj) => Some(obj.into_commit().unwrap()),
-            // ignore first commit
-            Err(_) => None,
-        };
-
-        let mut parents = Vec::new();
-        if let Some(parent) = parent_commit.as_ref() {
-            parents.push(parent);
-        }
-
-        let sig = self.repo.signature().unwrap();
-        let commit_msg = &commit.get_commit_message(cfg);
-
-        self.repo
-            .commit(
-                Some("HEAD"),
-                &sig,
-                &sig,
-                commit_msg,
-                &tree,
-                &parents[..],
-            )
-            .unwrap();
     }
 
     // won't bother with this for now
