67746b6c4d1dbd3b8ea0e732d059917d5dc9770e
nuttyCream <issanutty@gmail.com>
2025-11-04 18:05:27 -0800

    refactor(auth): switch to file-based token storage
    Replaced the `keyring` crate with a direct file-based approach for
    storing authentication tokens. This change removes the `keyring`
    dependency and simplifies token management by writing/reading tokens
    from a dedicated file within the user's configuration directory.
    
    The `token_path` function was introduced to determine the correct
    storage location.

--- src/auth.rs
+++ src/auth.rs
@@ -1,22 +1,41 @@
-use std::str::from_utf8;
+use std::{fs, path::PathBuf};
 
 use anyhow::Result;
-use keyring::Entry;
 
 pub fn store_token(token: &str) -> Result<()> {
     if token.is_empty() {
         return Err(anyhow::anyhow!("token cannot be empty"));
     }
 
-    let entry = Entry::new("gai", "token")?;
-    let secret = token.as_bytes();
+    let token_path = token_path()?;
 
-    entry.set_secret(secret)?;
+    fs::write(&token_path, token)?;
+
     Ok(())
 }
 
 pub fn get_token() -> Result<String> {
-    let entry = Entry::new("gai", "token")?;
-    let token = entry.get_secret()?;
-    Ok(from_utf8(&token)?.to_string())
+    let token_path = token_path()?;
+
+    Ok(fs::read_to_string(token_path)?.trim().to_string())
+}
+
+pub fn delete_token() -> Result<()> {
+    let token_path = token_path()?;
+
+    if token_path.exists() {
+        fs::remove_file(token_path)?;
+    }
+
+    Ok(())
+}
+
+fn token_path() -> Result<PathBuf> {
+    let cfg_dir =
+        directories::ProjectDirs::from("com", "nuttycream", "gai")
+            .ok_or_else(|| {
+                anyhow::anyhow!("Can't find the config directory")
+            })?;
+
+    Ok(cfg_dir.config_dir().join("token"))
 }
