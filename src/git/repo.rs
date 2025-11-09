036288970d26a31402c2cc5614b51fba25e49c18
nuttyCream <issanutty@gmail.com>
2025-11-09 03:25:17 -0800

    feat(git): Introduce structured repository status object

--- src/git/repo.rs
+++ src/git/repo.rs
@@ -1,5 +1,5 @@
 use anyhow::{Result, bail};
-use git2::{Repository, StatusOptions};
+use git2::{Repository, Status, StatusOptions};
 use std::collections::HashMap;
 use walkdir::WalkDir;
 
@@ -10,9 +10,27 @@ pub struct GaiGit {
     /// git2 based Repo
     pub repo: Repository,
 
+    pub status: GaiStatus,
+
     pub stage_hunks: bool,
     pub capitalize_prefix: bool,
     pub include_scope: bool,
+}
+
+/// helper to store paths for the files
+/// marked along status
+pub struct GaiStatus {
+    pub s_new: Vec<String>,
+    pub s_modified: Vec<String>,
+    pub s_deleted: Vec<String>,
+    // old -> new
+    pub s_renamed: Vec<(String, String)>,
+
+    // unstaged
+    pub u_new: Vec<String>,
+    pub u_modified: Vec<String>,
+    pub u_deleted: Vec<String>,
+    pub u_renamed: Vec<(String, String)>,
 }
 
 /// a sort of DiffDelta struct
@@ -65,13 +83,108 @@ impl GaiGit {
             bail!("no diffs");
         }
 
+        let status = Self::build_status(&repo)?;
+
         Ok(GaiGit {
             repo,
             files: Vec::new(),
+            status,
             stage_hunks,
             capitalize_prefix,
             include_scope,
         })
+    }
+
+    fn build_status(repo: &Repository) -> Result<GaiStatus> {
+        let mut status_opts = StatusOptions::new();
+
+        status_opts.include_untracked(true);
+
+        let statuses = repo.statuses(Some(&mut status_opts))?;
+
+        let mut status = GaiStatus {
+            s_new: Vec::new(),
+            s_modified: Vec::new(),
+            s_deleted: Vec::new(),
+            s_renamed: Vec::new(),
+            u_new: Vec::new(),
+            u_modified: Vec::new(),
+            u_deleted: Vec::new(),
+            u_renamed: Vec::new(),
+        };
+
+        for e in statuses.iter() {
+            if e.status().contains(Status::IGNORED) {
+                continue;
+            }
+
+            let path = e.path().unwrap_or("").to_string();
+
+            if e.status().contains(Status::INDEX_NEW) {
+                status.s_new.push(path.to_owned());
+            }
+
+            if e.status().contains(Status::INDEX_MODIFIED) {
+                status.s_modified.push(path.to_owned());
+            }
+
+            if e.status().contains(Status::INDEX_DELETED) {
+                status.s_deleted.push(path.to_owned());
+            }
+
+            if e.status().contains(Status::INDEX_RENAMED)
+                && let Some(diff) = e.head_to_index()
+            {
+                let old_path = diff
+                    .old_file()
+                    .path()
+                    .and_then(|p| p.to_str())
+                    .unwrap_or("");
+                let new_path = diff
+                    .new_file()
+                    .path()
+                    .and_then(|p| p.to_str())
+                    .unwrap_or("");
+                status.s_renamed.push((
+                    old_path.to_string(),
+                    new_path.to_string(),
+                ));
+            }
+
+            if e.status().contains(Status::WT_NEW) {
+                status.u_new.push(path.to_owned());
+            }
+
+            if e.status().contains(Status::WT_MODIFIED) {
+                status.u_modified.push(path.to_owned());
+            }
+
+            if e.status().contains(Status::WT_DELETED) {
+                status.u_deleted.push(path.to_owned());
+            }
+
+            if e.status().contains(Status::WT_RENAMED)
+                && let Some(diff) = e.index_to_workdir()
+            {
+                let old = diff
+                    .old_file()
+                    .path()
+                    .and_then(|p| p.to_str())
+                    .unwrap_or("");
+
+                let new = diff
+                    .new_file()
+                    .path()
+                    .and_then(|p| p.to_str())
+                    .unwrap_or("");
+
+                status
+                    .u_renamed
+                    .push((old.to_owned(), new.to_owned()));
+            }
+        }
+
+        Ok(status)
     }
 
     pub fn get_repo_tree(&self) -> String {
@@ -80,12 +193,12 @@ impl GaiGit {
 
         let mut repo_tree = String::new();
 
-        for entry in WalkDir::new(repo_root)
+        for e in WalkDir::new(repo_root)
             .into_iter()
             .filter_map(|e| e.ok())
             .filter(|e| e.file_type().is_file())
         {
-            if let Ok(rel_path) = entry.path().strip_prefix(repo_root)
+            if let Ok(rel_path) = e.path().strip_prefix(repo_root)
                 && !self.repo.status_should_ignore(rel_path).unwrap()
             {
                 repo_tree
@@ -96,71 +209,40 @@ impl GaiGit {
         repo_tree
     }
 
-    // this should be stored when we create_diffs
-    // todo: refactor
-    pub fn get_repo_status(&self) -> String {
-        let mut status_opts = StatusOptions::new();
-        status_opts.include_untracked(true);
-        let statuses = self.repo
-            .statuses(Some(&mut status_opts))
-            .expect("somehow failed to get statuses the second time around");
-
+    pub fn get_repo_status_as_str(&self) -> String {
         let mut staged = String::new();
         let mut unstaged = String::new();
 
-        for entry in statuses
-            .iter()
-            .filter(|e| e.status() != git2::Status::CURRENT)
-        {
-            let istatus = match entry.status() {
-                s if s.contains(git2::Status::INDEX_NEW) => 'A',
-                s if s.contains(git2::Status::INDEX_MODIFIED) => 'M',
-                s if s.contains(git2::Status::INDEX_DELETED) => 'D',
-                s if s.contains(git2::Status::INDEX_RENAMED) => 'R',
-                s if s.contains(git2::Status::INDEX_TYPECHANGE) => {
-                    'T'
-                }
-                _ => ' ',
-            };
+        for path in &self.status.s_new {
+            staged.push_str(&format!("A  {}\n", path));
+        }
 
-            let wstatus = match entry.status() {
-                s if s.contains(git2::Status::WT_NEW) => '?',
-                s if s.contains(git2::Status::WT_MODIFIED) => 'M',
-                s if s.contains(git2::Status::WT_DELETED) => 'D',
-                s if s.contains(git2::Status::WT_RENAMED) => 'R',
-                s if s.contains(git2::Status::WT_TYPECHANGE) => 'T',
-                _ => ' ',
-            };
+        for path in &self.status.s_modified {
+            staged.push_str(&format!("M  {}\n", path));
+        }
 
-            if entry.status().contains(git2::Status::IGNORED) {
-                continue;
-            }
+        for path in &self.status.s_deleted {
+            staged.push_str(&format!("D  {}\n", path));
+        }
 
-            let path = if let Some(diff) = entry.head_to_index() {
-                diff.new_file().path()
-            } else if let Some(diff) = entry.index_to_workdir() {
-                diff.old_file().path()
-            } else {
-                None
-            };
+        for (old, new) in &self.status.s_renamed {
+            staged.push_str(&format!("R  {} -> {}\n", old, new));
+        }
 
-            if let Some(path) = path {
-                let path_str = path.display().to_string();
+        for path in &self.status.u_new {
+            unstaged.push_str(&format!("? {}\n", path));
+        }
 
-                if istatus != ' ' {
-                    staged.push_str(&format!(
-                        "{}  {}\n",
-                        istatus, path_str
-                    ));
-                }
+        for path in &self.status.u_modified {
+            unstaged.push_str(&format!("M {}\n", path));
+        }
 
-                if wstatus != ' ' {
-                    unstaged.push_str(&format!(
-                        " {} {}\n",
-                        wstatus, path_str
-                    ));
-                }
-            }
+        for path in &self.status.u_deleted {
+            unstaged.push_str(&format!("D {}\n", path));
+        }
+
+        for (old, new) in &self.status.u_renamed {
+            unstaged.push_str(&format!("R {} -> {}\n", old, new));
         }
 
         let mut status_str = String::new();
