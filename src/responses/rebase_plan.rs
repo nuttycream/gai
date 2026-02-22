dcc7c8b426524f923161b6144231208b4ceb3098
nuttyCream <issanutty@gmail.com>
2026-02-22 06:01:16 -0800

    feat: impl parse rebase_plan_schema()

--- src/responses/rebase_plan.rs
+++ src/responses/rebase_plan.rs
@@ -1,9 +1,16 @@
-use crate::schema::rebase_plan::PlanOperationSchema;
+use crate::schema::rebase_plan::{
+    PlanOperationSchema, RebasePlanResponse,
+};
 
 /// extract rebase_plan from
-/// response
+/// response returns a list of
+/// operations
+/// TODO: impl validation
 pub fn parse_from_rebase_plan_schema(
     value: serde_json::Value
 ) -> anyhow::Result<Vec<PlanOperationSchema>> {
-    todo!()
+    let plan_resp =
+        serde_json::from_value::<RebasePlanResponse>(value)?;
+
+    Ok(plan_resp.operations)
 }
